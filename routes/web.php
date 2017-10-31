1fa4246fdca9e2f8f5d6f3553a54397e612cc774
Mohammed Zaki <mohammedzaki.dev@gmail.com>
2017-10-31 06:59:37 +0200

    remove un-needed routes

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@
 
 Auth::routes();
 
+Route::match(['get', 'post'], 'register', function(){
+    abort(404);
+});
+
 Route::get('/home', 'HomeController@index');
-
-Route::get('generator_builder', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@builder');
-
-Route::get('field_template', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@fieldTemplate');
-
-Route::post('generator_builder/generate', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@generate');
